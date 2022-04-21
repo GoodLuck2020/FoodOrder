0e478b3ac9d06f1671f108a7830921d2679b4cc7
GoodLuck2020 <topskill.dev@gmail.com>
2022-04-22 02:42:56 +0800

    Task2

--- modules/customer/components/subcomponents/OrderReview.js
+++ modules/customer/components/subcomponents/OrderReview.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import {ListItem, Text, Button, Icon} from 'react-native-elements';
 import Loading from 'react-native-loading-spinner-overlay';
 import {Auth} from 'aws-amplify';
@@ -8,7 +8,7 @@ import config from '../../config';
 import {Alert} from 'react-native';
 import Modal from 'react-native-modal';
 import {RFValue} from 'react-native-responsive-fontsize';
-import {TouchableOpacity} from 'react-native-gesture-handler';
+import Toast from 'react-native-easy-toast';
 import md5 from 'md5';
 
 function CardContainer({card, defaultCard, onSelect}) {
@@ -60,7 +60,7 @@ export default function OrderReview({navigation, route}) {
         profile.CustomerId,
     );
   };
-
+  const toastRef = useRef();
   const [visible, setVisible] = useState(false);
 
   const card = cards.length === 1 ? cards[0] : undefined;
@@ -147,98 +147,126 @@ export default function OrderReview({navigation, route}) {
       return;
     }
     try {
-      setLoading(true);
-      Stripe.setOptions({publishableKey: config.pk_test});
-      Auth.currentUserInfo().then(info => {
-        Stripe.createTokenWithCard({
-          number: cardItem.number,
-          cvc: cardItem.cvc,
-          expMonth: parseInt(cardItem.expiry.split('/')[0]),
-          expYear: parseInt(cardItem.expiry.split('/')[1]),
-          name: cardItem.name,
-        }).then(async tokeninfo => {
-          const body = {
-            amount: orderTotal,
-            token: tokeninfo.tokenId,
-            description:
-              info.username + ' has purchased ' + customerOrder.restaurantName,
-          };
-
-          const response = await fetch(
-            'https://veml8u1rjb.execute-api.us-west-1.amazonaws.com/beta/payment',
+      Alert.alert(
+          'Confirm',
+          'Are you sure you want to pay for this order?',
+          [
             {
-              method: 'POST', // *GET, POST, PUT, DELETE, etc.
-              mode: 'cors', // no-cors, *cors, same-origin
-              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-              credentials: 'same-origin', // include, *same-origin, omit
-              headers: {
-                'Content-Type': 'application/json',
-                Connection: 'keep-alive',
-                // ‘Content-Type’: ‘application/x-www-form-urlencoded’,
+              text: 'Yes',
+              onPress: () => {
+                setLoading(true);
+                Stripe.setOptions({publishableKey: config.pk_test});
+                Auth.currentUserInfo().then(info => {
+                  Stripe.createTokenWithCard({
+                    number: cardItem.number,
+                    cvc: cardItem.cvc,
+                    expMonth: parseInt(cardItem.expiry.split('/')[0]),
+                    expYear: parseInt(cardItem.expiry.split('/')[1]),
+                    name: cardItem.name,
+                  }).then(async tokeninfo => {
+                    const body = {
+                      amount: orderTotal,
+                      token: tokeninfo.tokenId,
+                      description:
+                          info.username + ' has purchased ' + customerOrder.restaurantName,
+                    };
+
+                    const response = await fetch(
+                        'https://veml8u1rjb.execute-api.us-west-1.amazonaws.com/beta/payment',
+                        {
+                          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                          mode: 'cors', // no-cors, *cors, same-origin
+                          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                          credentials: 'same-origin', // include, *same-origin, omit
+                          headers: {
+                            'Content-Type': 'application/json',
+                            Connection: 'keep-alive',
+                            // ‘Content-Type’: ‘application/x-www-form-urlencoded’,
+                          },
+                          redirect: 'follow', // manual, *follow, error
+                          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                          body: JSON.stringify(body), // body data type must match “Content-Type” header
+                        },
+                    );
+
+                    let data = await response.json();
+                    console.log('order detail =====>', info, restaurantInfo, customerOrder, profile);
+
+                    if (data.statusCode === 200) {
+                      let orderDetail = {
+                        incomingId: info.username,
+                        order: {
+                          customerName:
+                              profile.customerInfo.customerName.firstName +
+                              ' ' +
+                              profile.customerInfo.customerName.lastName,
+                          restaurantId: restaurantInfo.RestaurantId,
+                          restaurantName: customerOrder.restaurantName,
+                          menuId: '',
+                          menuItems: customerOrder.items,
+                          promoBought: [''],
+                          referralCode: '',
+                          orderTotal,
+                          timeOrderPlaced: new Date(),
+                          status: 'Awaiting Confirmation',
+                          timeOrderCompleted: ''
+                        },
+                      };
+
+                      let response = await fetch(
+                          'https://9yl3ar8isd.execute-api.us-west-1.amazonaws.com/beta/setuporders',
+                          {
+                            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                            mode: 'cors', // no-cors, *cors, same-origin
+                            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                            credentials: 'same-origin', // include, *same-origin, omit
+                            headers: {
+                              'Content-Type': 'application/json',
+                              Connection: 'keep-alive',
+                              // ‘Content-Type’: ‘application/x-www-form-urlencoded’,
+                            },
+                            redirect: 'follow', // manual, *follow, error
+                            referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                            body: JSON.stringify(orderDetail),
+                          },
+                      );
+
+                      console.log('payment =====>', await response.json());
+
+                      navigation.navigate('MenuDisplay', {paymentSuccess: true});
+
+                      navigation.navigate('Orders');
+                    } else {
+                      Alert.alert('Payment has failed');
+                    }
+                    setLoading(false);
+                  });
+                });
               },
-              redirect: 'follow', // manual, *follow, error
-              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-              body: JSON.stringify(body), // body data type must match “Content-Type” header
             },
-          );
-
-          let data = await response.json();
-          console.log('order detail =====>', info, restaurantInfo, customerOrder, profile);
-
-          if (data.statusCode === 200) {
-            let orderDetail = {
-              incomingId: info.username,
-              order: {
-                customerName:
-                    profile.customerInfo.customerName.firstName +
-                    ' ' +
-                    profile.customerInfo.customerName.lastName,
-                restaurantId: restaurantInfo.RestaurantId,
-                restaurantName: customerOrder.restaurantName,
-                menuId: '',
-                menuItems: customerOrder.items,
-                promoBought: [''],
-                referralCode: '',
-                orderTotal,
-                timeOrderPlaced: new Date(),
-                status: 'Awaiting Confirmation',
-                timeOrderCompleted: ''
-              },
-            };
-
-            let response = await fetch(
-              'https://9yl3ar8isd.execute-api.us-west-1.amazonaws.com/beta/setuporders',
-              {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                  'Content-Type': 'application/json',
-                  Connection: 'keep-alive',
-                  // ‘Content-Type’: ‘application/x-www-form-urlencoded’,
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify(orderDetail),
-              },
-            );
-
-            console.log('payment =====>', await response.json());
-
-            navigation.navigate('MenuDisplay', {paymentSuccess: true});
-
-            navigation.navigate('Orders');
-          } else {
-            Alert.alert('Payment has failed');
-          }
-          setLoading(false);
-        });
-      });
+            { text: 'No' },
+          ],
+          { cancelable: false },
+      );
     } catch (e) {
       setLoading(false);
     }
   };
+
+  const onPay = () => {
+    if (cards.length > 0) {
+      if (card) {
+        payment(card);
+      } else {
+        setVisible(true);
+      }
+    } else {
+      console.log('on pay =====>', cards.length);
+      toastRef.current.show('Please connect your card for pay.', 2000);
+    }
+  }
+
+  const orderStatus = 'Awaiting Confirmation';
 
   return (
     <>
@@ -252,25 +280,37 @@ export default function OrderReview({navigation, route}) {
         {route.params.order.items.map(orderItem => {
           return (
             <View key={orderItem.itemName}>
-              <ListItem
-                title={orderItem.itemName}
-                titleStyle={styles.orderItemTitle}
-                rightTitle={
-                  '$' + orderItem.price + ' * ' + (orderItem.quantity ?? 1)
-                }
-                subtitle={orderItem.specialInstructions}
-                //subtitle={getOptions(orderItem) || false}
-                subtitleStyle={styles.orderItemDescription}
-                containerStyle={styles.orderItemContainer}
-                // onPress={() => console.log('Pressed')}
-                bottomDivider
-              />
+              <View style={styles.itemWrapper}>
+                <View style={{width: '60%'}}>
+                  <Text style={styles.orderItemTitle}>{orderItem.itemName}</Text>
+                  <Text style={styles.orderItemDescription}>{orderItem.specialInstructions}</Text>
+                </View>
+                <View style={{width: '40%'}}>
+                  <Text style={styles.orderPriceText}>{
+                    '$' + orderItem.price + ' * ' + (orderItem.quantity ?? 1)
+                  }</Text>
+                  <Text style={styles.orderStatusText}>{orderStatus}</Text>
+                </View>
+              </View>
+              {/*<ListItem*/}
+              {/*  title={orderItem.itemName}*/}
+              {/*  titleStyle={styles.orderItemTitle}*/}
+              {/*  rightTitle={*/}
+              {/*    '$' + orderItem.price + ' * ' + (orderItem.quantity ?? 1)*/}
+              {/*  }*/}
+              {/*  subtitle={orderItem.specialInstructions}*/}
+              {/*  //subtitle={getOptions(orderItem) || false}*/}
+              {/*  subtitleStyle={styles.orderItemDescription}*/}
+              {/*  containerStyle={styles.orderItemContainer}*/}
+              {/*  // onPress={() => console.log('Pressed')}*/}
+              {/*  bottomDivider*/}
+              {/*/>*/}
             </View>
           );
         })}
         <Text style={styles.itemOrderTotalTitle}>Order Total</Text>
         <View style={styles.orderTotalContainer}>
-          <ListItem title="Subtotal:" rightTitle={'$' + orderSubtotal} />
+          <ListItem title="Subtotal:" rightTitle={'$' + (orderSubtotal ? orderSubtotal.toFixed(2) : '0.0')} />
           {/* TODO: GET FEES AND SALES TAX FROM DATA */}
           <ListItem
             title="Delivery Fees:"
@@ -284,15 +324,16 @@ export default function OrderReview({navigation, route}) {
       {/* <Button title={'Proceed To Checkout $' + orderSubtotal} /> */}
       {/* TODO: CREATE AND NAVIGATE TO PAYMENT SCREEN */}
       <Button
-        title={'Pay $' + orderTotal}
+        title={'Pay $' + (orderTotal ? orderTotal.toFixed(2) : '0.0')}
         buttonStyle={styles.buttonStyle}
-        onPress={() => (card ? payment(card) : setVisible(true))}
+        onPress={onPay}
       />
       <Loading visible={loading} />
       <Modal
         isVisible={visible}
         onBackdropPress={() => setVisible(false)}
-        style={{justifyContent: 'flex-end'}}>
+        style={styles.bottomModal}
+      >
         <View style={styles.modalInside}>
           {cards.map(item => (
             <CardContainer
@@ -313,6 +354,7 @@ export default function OrderReview({navigation, route}) {
           />
         </View>
       </Modal>
+      <Toast ref={toastRef} positionValue={260} position="bottom"/>
     </>
   );
 }
@@ -341,7 +383,6 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: '#fff',
   },
-  orderItemTitle: {},
   orderItemDescription: {
     fontSize: 12,
     color: '#111',
@@ -353,12 +394,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#F86D64',
     paddingTop: 15,
     paddingBottom: 15,
+    marginHorizontal: 15
+  },
+  bottomModal: {
+    justifyContent: 'flex-end',
+    margin: 0,
   },
   modalInside: {
     backgroundColor: 'white',
-    padding: 5,
+    padding: 10,
     paddingTop: 15,
-    paddingBottom: 15,
+    paddingBottom: 30,
     borderTopRightRadius: 15,
     borderTopLeftRadius: 15,
   },
@@ -387,4 +433,26 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#F86D64',
   },
+  itemWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: '#979797',
+    padding: 20
+  },
+  orderItemTitle: {
+    fontSize: 17,
+  },
+  orderPriceText: {
+    fontSize: 17,
+    color: 'black',
+    opacity: 0.5,
+    textAlign: 'right'
+  },
+  orderStatusText: {
+    fontSize: 12,
+    color: '#111',
+    textAlign: 'right'
+  }
 });
